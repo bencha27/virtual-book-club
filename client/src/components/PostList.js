ecc4e6c5773aea54f341d4d5077509b62ca0803c
bencha27 <115957643+bencha27@users.noreply.github.com>
2023-03-15 10:26:32 -0600

    Debugging /posts route

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -11,12 +11,12 @@ export default function PostList({ posts }) {
       <div className="row justify-content-center">
         {posts && 
           posts.map((post) => (
-            // <div key={post._id} className="col-md-10">
+            <div key={post._id} className="col-md-10">
               <div className="card mb-3" >
                 <div className="card-header">
                   <h4 className="card-title">{post.postTitle}</h4>
                   <h5 className="card-subtitle text-muted">
-                    By <Link to={`/user/${post.user._id}`}>{post.user.username}</Link>
+                    {/* By <Link to={`/user/${post.user._id}`}>{post.user.username}</Link> */}
                   <span style={{ float: "right" }}>
                       {`${new Date(Number.parseInt(post.createdAt)).toDateString()}`}
                   </span>
@@ -28,7 +28,7 @@ export default function PostList({ posts }) {
                   ) : (
                     <p>{post.postBody}</p>
                   )}
-                  {/* <Link to={`/posts/${post._id}`}>View post</Link> */}
+                  <Link to={`/posts/${post._id}`}>View post</Link>
                 </div>
               </div>
             // </div>
